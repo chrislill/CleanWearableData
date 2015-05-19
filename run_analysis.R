1df6516b336ee701442725c61bfdf94bc21c48ac
Chris Lill <chris_lill_uk@hotmail.com>
2015-05-19 11:42:17 +0100

    Completed script that outputs a dataset of averages

--- run_analysis.R
+++ run_analysis.R
@@ -19,11 +19,11 @@ subject.train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 x.train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y.train <- read.table("UCI HAR Dataset/train/Y_train.txt")
 
-# Create a data frame with a row for each sample
+# Create a data frame table with a row for each sample
 samples.count <- nrow(subject.test)+nrow(subject.train)
-samples <- cbind(1:samples.count,
-                     rbind(subject.test, subject.train),
-                     rbind(y.test, y.train))
+samples <- tbl_df(cbind(1:samples.count,
+                        rbind(subject.test, subject.train),
+                        rbind(y.test, y.train)))
 names(samples) <- c("id", "subject", "activity")
 samples$activity <- factor(samples$activity, 
                               labels = as.character(activity[,2]))
@@ -33,9 +33,31 @@ samples$activity <- factor(samples$activity,
 features.mean <- features[grep("mean()", features$V2, fixed=TRUE),]
 features.std <- features[grep("std()", features$V2, fixed=TRUE),]
 features.name <- gsub("-mean()", "", features.mean$V2, fixed=TRUE)
-features.col <- data.frame(features.name, features.mean$V1, features.std$V1)
-names(features.col) <- c("name", "mean", "std")
+features.col <- data.frame(name = features.name, 
+                           mean = features.mean$V1, 
+                           std = features.std$V1)
 
-# Gather the mean values for each sample
-#measurements <- gather(x.test, "feature", "mean", features.col$mean)
-#unique(measurements$feature)
+# Create a wide data sets for mean and std
+x.mean <- rbind(x.test[, features.col$mean], x.train[, features.col$mean])
+x.std <- rbind(x.test[, features.col$std], x.train[, features.col$std])
+names(x.mean) <- features.col$name
+names(x.std) <- features.col$name
+
+# Join the samples and x.mean datasets, and then gather the measurements
+samples <- cbind(samples, x.mean)
+measurements <- gather(samples, "measure", "mean", -(id:activity))
+
+# Gather the std measurements, and then join to the main dataset
+measurements.std <- gather(x.std, "measure", "std")
+measurements <- cbind(measurements, std = measurements.std$std)
+
+# Produce the averages of each measure grouped by activity and subject
+averages <- measurements %>%
+    group_by(activity, subject, measure) %>%
+    summarise(mean = mean(mean)) 
+
+# Write the data set out as a text file
+write.table(averages, "averages.txt", row.name=FALSE)
+
+
+
